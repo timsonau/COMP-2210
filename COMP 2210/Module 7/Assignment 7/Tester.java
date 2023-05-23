4e9364ea8d46cb783e2208183eb514d5c9a1005f
timsonau <117775073+timsonau@users.noreply.github.com>
2023-05-23 08:41:17 -0500

    Updated files and directories

--- COMP 2210/Module 7/Assignment 7/Tester.java
+++
@@ -1,7 +0,0 @@
-public class Tester {
-    public static void main(String[] args) {
-        String word = "John";
-        System.out.println(word.substring(0, 4));
-        MarkovModel MM = new MarkovModel(K, sourceText)
-    }
-}
